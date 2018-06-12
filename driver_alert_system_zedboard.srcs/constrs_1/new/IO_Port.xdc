b57bd23892f6a42594e45532777a3fcc4fa01430
Justin, Chang-Qi Zhang <justin840727@gmail.com>
2018-06-12 18:03:28 +0800

    first commit

---
+++ driver_alert_system_zedboard.srcs/constrs_1/new/IO_Port.xdc
@@ -0,0 +1,21 @@
+set_property IOSTANDARD LVCMOS33 [get_ports CAN_0_0_rx]
+set_property IOSTANDARD LVCMOS33 [get_ports CAN_0_0_tx]
+set_property IOSTANDARD LVCMOS33 [get_ports {led2_sp1_tri_o[2]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {led2_sp1_tri_o[1]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {led2_sp1_tri_o[0]}]
+set_property IOSTANDARD LVCMOS25 [get_ports {sw_2bit_tri_i[1]}]
+set_property IOSTANDARD LVCMOS25 [get_ports {sw_2bit_tri_i[0]}]
+set_property IOSTANDARD LVCMOS33 [get_ports IIC_0_0_scl_io]
+set_property IOSTANDARD LVCMOS33 [get_ports IIC_0_0_sda_io]
+set_property PACKAGE_PIN L21 [get_ports CAN_0_0_rx]
+set_property PACKAGE_PIN L22 [get_ports CAN_0_0_tx]
+set_property PACKAGE_PIN T22 [get_ports {led2_sp1_tri_o[0]}]
+set_property PACKAGE_PIN T21 [get_ports {led2_sp1_tri_o[1]}]
+set_property PACKAGE_PIN U22 [get_ports {led2_sp1_tri_o[2]}]
+
+
+set_property PACKAGE_PIN G22 [get_ports {sw_2bit_tri_i[1]}]
+set_property PACKAGE_PIN F22 [get_ports {sw_2bit_tri_i[0]}]
+
+set_property PACKAGE_PIN Y11 [get_ports IIC_0_0_scl_io]
+set_property PACKAGE_PIN AA11 [get_ports IIC_0_0_sda_io]
---
+++ driver_alert_system_zedboard.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -0,0 +1,144 @@
+//Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2018.1 (lin64) Build 2188600 Wed Apr  4 18:39:19 MDT 2018
+//Date        : Mon Jun 11 11:52:24 2018
+//Host        : justin-pev running 64-bit Ubuntu 16.04.4 LTS
+//Command     : generate_target design_1_wrapper.bd
+//Design      : design_1_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_1_wrapper
+   (CAN_0_0_rx,
+    CAN_0_0_tx,
+    DDR_0_addr,
+    DDR_0_ba,
+    DDR_0_cas_n,
+    DDR_0_ck_n,
+    DDR_0_ck_p,
+    DDR_0_cke,
+    DDR_0_cs_n,
+    DDR_0_dm,
+    DDR_0_dq,
+    DDR_0_dqs_n,
+    DDR_0_dqs_p,
+    DDR_0_odt,
+    DDR_0_ras_n,
+    DDR_0_reset_n,
+    DDR_0_we_n,
+    FIXED_IO_0_ddr_vrn,
+    FIXED_IO_0_ddr_vrp,
+    FIXED_IO_0_mio,
+    FIXED_IO_0_ps_clk,
+    FIXED_IO_0_ps_porb,
+    FIXED_IO_0_ps_srstb,
+    IIC_0_0_scl_io,
+    IIC_0_0_sda_io,
+    led2_sp1_tri_o,
+    sw_2bit_tri_i);
+  input CAN_0_0_rx;
+  output CAN_0_0_tx;
+  inout [14:0]DDR_0_addr;
+  inout [2:0]DDR_0_ba;
+  inout DDR_0_cas_n;
+  inout DDR_0_ck_n;
+  inout DDR_0_ck_p;
+  inout DDR_0_cke;
+  inout DDR_0_cs_n;
+  inout [3:0]DDR_0_dm;
+  inout [31:0]DDR_0_dq;
+  inout [3:0]DDR_0_dqs_n;
+  inout [3:0]DDR_0_dqs_p;
+  inout DDR_0_odt;
+  inout DDR_0_ras_n;
+  inout DDR_0_reset_n;
+  inout DDR_0_we_n;
+  inout FIXED_IO_0_ddr_vrn;
+  inout FIXED_IO_0_ddr_vrp;
+  inout [53:0]FIXED_IO_0_mio;
+  inout FIXED_IO_0_ps_clk;
+  inout FIXED_IO_0_ps_porb;
+  inout FIXED_IO_0_ps_srstb;
+  inout IIC_0_0_scl_io;
+  inout IIC_0_0_sda_io;
+  output [2:0]led2_sp1_tri_o;
+  input [1:0]sw_2bit_tri_i;
+
+  wire CAN_0_0_rx;
+  wire CAN_0_0_tx;
+  wire [14:0]DDR_0_addr;
+  wire [2:0]DDR_0_ba;
+  wire DDR_0_cas_n;
+  wire DDR_0_ck_n;
+  wire DDR_0_ck_p;
+  wire DDR_0_cke;
+  wire DDR_0_cs_n;
+  wire [3:0]DDR_0_dm;
+  wire [31:0]DDR_0_dq;
+  wire [3:0]DDR_0_dqs_n;
+  wire [3:0]DDR_0_dqs_p;
+  wire DDR_0_odt;
+  wire DDR_0_ras_n;
+  wire DDR_0_reset_n;
+  wire DDR_0_we_n;
+  wire FIXED_IO_0_ddr_vrn;
+  wire FIXED_IO_0_ddr_vrp;
+  wire [53:0]FIXED_IO_0_mio;
+  wire FIXED_IO_0_ps_clk;
+  wire FIXED_IO_0_ps_porb;
+  wire FIXED_IO_0_ps_srstb;
+  wire IIC_0_0_scl_i;
+  wire IIC_0_0_scl_io;
+  wire IIC_0_0_scl_o;
+  wire IIC_0_0_scl_t;
+  wire IIC_0_0_sda_i;
+  wire IIC_0_0_sda_io;
+  wire IIC_0_0_sda_o;
+  wire IIC_0_0_sda_t;
+  wire [2:0]led2_sp1_tri_o;
+  wire [1:0]sw_2bit_tri_i;
+
+  IOBUF IIC_0_0_scl_iobuf
+       (.I(IIC_0_0_scl_o),
+        .IO(IIC_0_0_scl_io),
+        .O(IIC_0_0_scl_i),
+        .T(IIC_0_0_scl_t));
+  IOBUF IIC_0_0_sda_iobuf
+       (.I(IIC_0_0_sda_o),
+        .IO(IIC_0_0_sda_io),
+        .O(IIC_0_0_sda_i),
+        .T(IIC_0_0_sda_t));
+  design_1 design_1_i
+       (.CAN_0_0_rx(CAN_0_0_rx),
+        .CAN_0_0_tx(CAN_0_0_tx),
+        .DDR_0_addr(DDR_0_addr),
+        .DDR_0_ba(DDR_0_ba),
+        .DDR_0_cas_n(DDR_0_cas_n),
+        .DDR_0_ck_n(DDR_0_ck_n),
+        .DDR_0_ck_p(DDR_0_ck_p),
+        .DDR_0_cke(DDR_0_cke),
+        .DDR_0_cs_n(DDR_0_cs_n),
+        .DDR_0_dm(DDR_0_dm),
+        .DDR_0_dq(DDR_0_dq),
+        .DDR_0_dqs_n(DDR_0_dqs_n),
+        .DDR_0_dqs_p(DDR_0_dqs_p),
+        .DDR_0_odt(DDR_0_odt),
+        .DDR_0_ras_n(DDR_0_ras_n),
+        .DDR_0_reset_n(DDR_0_reset_n),
+        .DDR_0_we_n(DDR_0_we_n),
+        .FIXED_IO_0_ddr_vrn(FIXED_IO_0_ddr_vrn),
+        .FIXED_IO_0_ddr_vrp(FIXED_IO_0_ddr_vrp),
+        .FIXED_IO_0_mio(FIXED_IO_0_mio),
+        .FIXED_IO_0_ps_clk(FIXED_IO_0_ps_clk),
+        .FIXED_IO_0_ps_porb(FIXED_IO_0_ps_porb),
+        .FIXED_IO_0_ps_srstb(FIXED_IO_0_ps_srstb),
+        .IIC_0_0_scl_i(IIC_0_0_scl_i),
+        .IIC_0_0_scl_o(IIC_0_0_scl_o),
+        .IIC_0_0_scl_t(IIC_0_0_scl_t),
+        .IIC_0_0_sda_i(IIC_0_0_sda_i),
+        .IIC_0_0_sda_o(IIC_0_0_sda_o),
+        .IIC_0_0_sda_t(IIC_0_0_sda_t),
+        .led2_sp1_tri_o(led2_sp1_tri_o),
+        .sw_2bit_tri_i(sw_2bit_tri_i));
+endmodule
